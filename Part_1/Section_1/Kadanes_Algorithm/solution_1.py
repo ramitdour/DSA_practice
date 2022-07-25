c4556b7e37d3ef7fbc5a89de04697d4cbe5f5a4c
ramitdour <ramit.dour@gmail.com>
2022-07-25 23:29:40 +0530

    Kadanes_Algorithm runtime error

--- Part_1/Section_1/Kadanes_Algorithm/solution_1.py
+++ Part_1/Section_1/Kadanes_Algorithm/solution_1.py
@@ -4,13 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        largest_sum = 0 
+        largest_sum = nums[0]
         len_arr = len(nums)
         sub_arr = []
 
-        for i in range(len_arr):
+        # for i in range(len_arr):
+        #     for k in range(len_arr - i):
+        #         x = nums[k: (len_arr - ( len_arr - i) + 1)]
+        #         sub_arr_sum = sum(x)
+        #         if (largest_sum < sub_arr_sum): 
+        #             largest_sum = sub_arr_sum
+        #             sub_arr = x
+
+        for i in range(len_arr , -1 , -1):
             for k in range(len_arr - i):
-                x = nums[i: (len_arr - k +1)]
+                # print(nums[k : k + i +1])
+                x = nums[k : k + i +1]
                 sub_arr_sum = sum(x)
                 if (largest_sum < sub_arr_sum): 
                     largest_sum = sub_arr_sum
@@ -26,6 +35,10 @@ s1 = Solution()
 print(s1.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 print(s1.maxSubArray([1]))
 print(s1.maxSubArray([5,4,-1,7,8]))
+print(s1.maxSubArray([-1,0,-2]))
+
+
+# print(s1.maxSubArray(lx))
 
 # Definition of lexicography
 # 1 : the editing or making of a dictionary. 

b01c48753c122c81ae64afd9f22c70b2540ccc07
ramitdour <ramit.dour@gmail.com>
2022-07-25 23:05:33 +0530

    Kadanes_Algorithm

---
+++ Part_1/Section_1/Kadanes_Algorithm/solution_1.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        largest_sum = 0 
+        len_arr = len(nums)
+        sub_arr = []
+
+        for i in range(len_arr):
+            for k in range(len_arr - i):
+                x = nums[i: (len_arr - k +1)]
+                sub_arr_sum = sum(x)
+                if (largest_sum < sub_arr_sum): 
+                    largest_sum = sub_arr_sum
+                    sub_arr = x
+
+        return largest_sum
+
+
+
+
+s1 = Solution()
+
+print(s1.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+print(s1.maxSubArray([1]))
+print(s1.maxSubArray([5,4,-1,7,8]))
+
+# Definition of lexicography
+# 1 : the editing or making of a dictionary. 

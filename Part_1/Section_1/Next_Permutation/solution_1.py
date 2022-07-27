c27b114bdd2b8b216f5cfa92756c2b13202d0a54
ramitdour <ramit.dour@gmail.com>
2022-07-27 23:46:36 +0530

    next permutation que only

---
+++ Part_1/Section_1/Next_Permutation/solution_1.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+
+
+s1 = Solution()
+
+print(s1.nextPermutation([1, 2, 3]))
+print(s1.nextPermutation([3, 2, 1]))
+print(s1.nextPermutation([1, 1, 5]))
+
+# Definition of lexicography
+# 1 : the editing or making of a dictionary. 

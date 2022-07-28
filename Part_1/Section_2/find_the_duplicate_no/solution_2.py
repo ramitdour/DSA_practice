deda12eb97a225bc4a1c86ddbc641debb4be6f03
ramitdour <ramit.dour@gmail.com>
2022-07-28 16:26:16 +0530

    tortoise method

---
+++ Part_1/Section_2/find_the_duplicate_no/solution_2.py
@@ -0,0 +1,36 @@
+from typing import List
+from large_ds import l1
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow_pointer = nums[0]
+        fast_pointer = nums[0]
+
+        while True:
+            slow_pointer = nums[slow_pointer]
+            fast_pointer = nums[nums[fast_pointer]]
+
+            if slow_pointer == fast_pointer:
+                print("s f b",slow_pointer,fast_pointer)
+                break
+        
+        
+        fast_pointer = nums[0]
+
+        print("s f z",slow_pointer,fast_pointer)
+
+        while slow_pointer != fast_pointer :
+            slow_pointer = nums[slow_pointer]
+            fast_pointer = nums[fast_pointer]  # made mistake here of moving fast counter by speed of 2x
+
+        print("s f r",slow_pointer,fast_pointer)
+
+        return slow_pointer
+
+s1 = Solution()
+
+print(s1.findDuplicate([1,3,4,2,2]))
+print(s1.findDuplicate([3,1,3,4,2]))
+print(s1.findDuplicate([2,5,9,6,9,3,8,9,7,1]))
+
+print(s1.findDuplicate(l1))
